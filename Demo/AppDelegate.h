c8671c8b7fc88f671b2178588cf3b9403cc30518
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2016-07-23 18:27:20 +0500

    Resolved code duplication issue...

--- Demo/AppDelegate.h
+++ Demo/AppDelegate.h
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.h
-//  UIViewTouchHighlightingExample
+//  Demo
 //
-//  Created by Muhammad Tayyab Akram on 08/12/2015.
-//  Copyright © 2015 Muhammad Tayyab Akram. All rights reserved.
+//  Created by Muhammad Tayyab Akram on 23/07/2016.
+//  Copyright © 2016 Muhammad Tayyab Akram. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
---
+++ Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Muhammad Tayyab Akram on 23/07/2016.
+//  Copyright © 2016 Muhammad Tayyab Akram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
